1b51374e1b7f7de740c2782e027f143dae28e108
Matt <matt.taylor2010@hotmail.com>
2025-08-31 16:53:20 +0100

    Automated commit – 2025-08-31 16:53:20 GMTST

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
+<?php
 <?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -6,23 +11,23 @@
     <meta charset="UTF-8">
     <title>Marvellous Maine Coons</title>
     <meta name="viewport" content="width=device-width, initial-scale=1"> <!-- Add this line -->
-    <link rel="stylesheet" href="/css/styles.css">
+    <link rel="stylesheet" href="./css/styles.css">
 </head>
 <body>
 
-<?php include __DIR__ . '/includes/header.php'; ?>
+<?php include __DIR__ . './includes/header.php'; ?>
 
 <main>
     <h2>Meet Our Maine Coons</h2>
     <p class="description">We raise beautiful, gentle Maine Coon kittens with love and care. Browse our current litters and learn more about each kitten’s personality.</p>
 
     <div class="kitten-highlight">
-        <img src="/images/cooper.jpg" alt="Cooper the Maine Coon">
+        <img src="./images/cooper.jpg" alt="Cooper the Maine Coon">
         <p>Cooper the Maine Coon</p>
     </div>
 </main>
 
-<?php include __DIR__ . '/includes/footer.php'; ?>
+<?php include __DIR__ . './includes/footer.php'; ?>
 
 </body>
 </html>
